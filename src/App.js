d723b2c4c6e7b89f0a65d11aae4681aaabd769d8
Zack Webster <33168087+webster1000@users.noreply.github.com>
2019-01-17 17:39:25 +0530

    Added Bootstrap, jQuery & popper.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
       </div>
     );
   }
